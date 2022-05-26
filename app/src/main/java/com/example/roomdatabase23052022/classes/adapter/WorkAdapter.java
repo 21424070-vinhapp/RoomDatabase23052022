e660198d19b195eb8300f3afef7c976dfc0b1dd2
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-26 15:21:37 +0700

    update code

---
+++ app/src/main/java/com/example/roomdatabase23052022/classes/adapter/WorkAdapter.java
@@ -0,0 +1,16 @@
+package com.example.roomdatabase23052022.classes.adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class WorkAdapter {
+
+    class WorkViewHolder extends RecyclerView.ViewHolder{
+
+        public WorkViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
